4ceebc01f148d7466c097190dfb16cb37f114ff5
Ata4fm <Ata4fm@gmail.com>
2022-03-19 10:37:48 +0330

    changed directory
    i created new directory and i moved my urls to  new template

--- myweb/settings.py
+++ myweb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
