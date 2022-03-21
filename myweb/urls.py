ac78e682de1f512d570dc4563b4ac317c502ad5e
Ata4fm <Ata4fm@gmail.com>
2022-03-21 13:44:51 +0330

    urls are done
    I completed website and blog urls

--- myweb/urls.py
+++ myweb/urls.py
@@ -19,10 +19,13 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from website import urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('website.urls'))
+    path('',include('website.urls')),
+    path('blog',include('blog.urls'))
 
     
 ]
