20a47fcaf6cd688531c51a0757d4a60a34cc824f
Ata4fm <Ata4fm@gmail.com>
2022-03-24 00:51:36 +0430

    table on website
    i created new class table on wesbite app with new attributes

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 
+from pickle import TRUE
 from django.db import models
 
 
 class Post(models.Model):
         Title=models.CharField(max_length=100)
         Content=models.TextField()
+        counted_view = models.IntegerField(default=0) 
+        status = models.BooleanField(default=False)
+        created_date=models.DateTimeField(auto_now_add=True)
+        updated_date=models.DateTimeField(auto_now=True)
+        published_date=models.DateTimeField(null=True)
