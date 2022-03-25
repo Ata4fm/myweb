924fa953ad6e958aaa9c31cfd184b4be6a23d18e
Ata4fm <Ata4fm@gmail.com>
2022-03-25 17:00:53 +0430

    dynamic data on html web page

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('',blog_view,name='index'),
-    path('single',blog_single,name='single') 
+    path('<int:pid>',blog_single,name='single') 
 
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-
+from multiprocessing import context
+from django.shortcuts import render , get_object_or_404
+from blog.models import Post
 
 
 from django.http import HttpResponse
 # Create your views here.
+
+
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
+    posts=Post.objects.filter(status=1)
+    context= {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
+
+
+def blog_single(request,pid):
+    posts=Post.objects.filter(status=1)
+    post = get_object_or_404(posts,pk=pid)
+    context = {'post': post}
+    return render(request,'blog/blog-single.html',context) 
